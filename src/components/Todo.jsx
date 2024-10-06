86db393e44beca5b67d4fd1d7baf9c79ae42da42
rohullahkyhan <kyhanrohullah22@gamil.com>
2024-10-06 10:10:22 +0430

    update todo commited

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -8,16 +8,35 @@ const Todo = () => {
     if (!input) {
       return;
     }
-    setTodo("");
-    setTodo([...todo, input]);
+
+    const newTodo = {
+      id: Math.random() * 10,
+      content: input,
+      completed: false,
+    };
+
+    setTodo((prevTodo) => {
+      return [...prevTodo, newTodo];
+    });
+
     setInput("");
   };
+
+  const handleComplated = (id) => {
+    const newTodo = todo.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+
+    setTodo(newTodo);
+  };
+
   return (
     <div className="container">
       <div>
         <div className="input--box">
           <input
             type="text"
+            data-name="inputText"
             placeholder="Add a new task..."
             value={input}
             onChange={(e) => setInput(e.target.value)}
@@ -27,26 +46,39 @@ const Todo = () => {
           </button>
         </div>
 
-        {todo.length > 0 ? (
-          <ul className="todo--list">
-            {todo.map((t, index) => (
-              <li key={index}>
-                <span>
-                  <input type="checkbox" /> {t}
-                </span>
-                <button
-                  onClick={() =>
-                    setTodo(todo.filter((_, item) => item !== index))
-                  }
-                >
-                  X
-                </button>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <span>Note Found.</span>
-        )}
+        {/* {todo.length > 0 ? ( */}
+        <ul className="todo--list">
+          {todo.map((t, index) => (
+            <li
+              key={index}
+              style={
+                t.completed === true
+                  ? { textDecoration: "line-through", color: "gray" }
+                  : {}
+              }
+            >
+              <span>
+                <input
+                  data-name="checkbox"
+                  type="checkbox"
+                  checked={t.completed}
+                  onChange={() => handleComplated(t.id)}
+                />{" "}
+                <span>{t.content}</span>
+              </span>
+              <button
+                onClick={() =>
+                  setTodo(todo.filter((_, item) => item !== index))
+                }
+              >
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
+        {/* ) : (
+          <span>Note Found.</span> */}
+        {/* )} */}
       </div>
     </div>
   );
